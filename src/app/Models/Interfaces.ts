04e1510761f30a9248f34387dc4c9435c43740ca
David2003006 <davidalejandro2003@yahoo.com>
2024-11-05 18:09:26 -0600

    Actualizacion 05/11/2024

--- src/app/Models/Interfaces.ts
+++ src/app/Models/Interfaces.ts
@@ -4,3 +4,19 @@ export interface Menu {
     titulo: string;
 
 }
+
+export interface Producto{
+    CategoriaID: string,
+    Nombre : string,
+    Precio: number,
+    ProductoID: string,
+    descripcion: string,
+    RentaID: string,
+    imagen: string
+}
+
+export interface Renta{
+
+    Nombre: string,
+    RentaID: string
+}
--- src/app/Views/detalle-renta/detalle-renta.page.ts
+++ src/app/Views/detalle-renta/detalle-renta.page.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone';
 import { HeaderComponent } from '../header/header.component';
+import { Producto } from 'src/app/Models/Interfaces';
+import { FirestoreDatabaseService } from 'src/app/Services/firestore-database.services';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalle-renta',
@@ -14,9 +17,26 @@ import { HeaderComponent } from '../header/header.component';
 })
 export class DetalleRentaPage implements OnInit {
 
-  constructor() { }
+  @Input() productoId!: string; // Recibe el ID del producto
+  producto?: Producto;
+
+  constructor(private fireStoreServices: FirestoreDatabaseService, 
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id'); // 'id' debe coincidir con el nombre en la ruta
+    console.log("el id de la guitarra es:"+ id)
+    if (id) {
+      this.obtenerProducto(id);
+    }
+  }
+
+  obtenerProducto(id: string) {
+    this.fireStoreServices.getDoc<Producto>('Producto', id).subscribe((producto) => {
+      console.log(producto)
+      this.producto = producto;
+    });
   }
 
 }
