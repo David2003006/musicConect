123495014527682ac8be32a8796feb460e756d35
JAIME ALEJANDRO GONZALEZ PACHECO <jaime.gonzalezpc@uanl.edu.mx>
2024-11-16 17:45:34 -0600

    Mejoras visuales y implementar secciones contenido curso

--- src/app/Views/contenido-curso/contenido-curso.page.ts
+++ src/app/Views/contenido-curso/contenido-curso.page.ts
@@ -3,6 +3,10 @@ import { IonContent, IonIcon } from '@ionic/angular/standalone';
 import { HeaderComponent } from '../header/header.component';
 import { addIcons } from 'ionicons';
 import { caretDown } from 'ionicons/icons';
+import { CommonModule } from '@angular/common';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+
 
 @Component({
   selector: 'app-contenido-curso',
@@ -12,7 +16,16 @@ import { caretDown } from 'ionicons/icons';
   imports: [
     IonContent,
     HeaderComponent,
-    IonIcon
+    IonIcon,
+    CommonModule
+    
+  ],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({ opacity: 0 })),
+      state('*', style({ opacity: 1 })),
+      transition('void <=> *', [animate('900ms ease-in-out')])
+    ])
   ]
 })
 export class ContenidoCursoPage implements OnInit {
@@ -25,5 +38,18 @@ export class ContenidoCursoPage implements OnInit {
 
   ngOnInit() {
   }
+
+    // Arreglo que representa las secciones
+    sections = [1, 2, 3, 4]; // Puedes añadir tantas secciones como necesites
+
+    // Estado de visibilidad para cada sección
+    visibleSections: boolean[] = Array(this.sections.length).fill(false);
+  
+    // Alterna la visibilidad de una sección específica
+    toggleVisibility(index: number): void {
+      this.visibleSections[index] = !this.visibleSections[index];
+    }
+
+  
 
 }
