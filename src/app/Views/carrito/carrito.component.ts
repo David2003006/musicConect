a0262f8bcafe8c2e36ddc02a646ce0b38ea58d9f
David2003006 <davidalejandro2003@yahoo.com>
2024-11-18 22:23:50 -0600

    Agregamos la funcionalidad del icono del carrito

--- src/app/Views/carrito/carrito.component.ts
+++ src/app/Views/carrito/carrito.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IonPopover, IonList, IonItem } from '@ionic/angular/standalone';
 import { NavController } from '@ionic/angular';
+import { CarritoService } from 'src/app/Services/carrito.service';
+import { Carrito } from 'src/app/Models/Interfaces';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-carrito',
@@ -9,18 +12,34 @@ import { NavController } from '@ionic/angular';
   standalone: true,
   imports: [
     IonPopover,
-    IonList, IonItem],
+    IonList, IonItem,
+    CommonModule
+  ],
 })
-export class CarritoComponent  implements OnInit {
+export class CarritoComponent implements OnInit {
+  productosCarrito: Carrito[] = [];
+  totalCarrito: number = 0;
 
-  constructor(private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController, private carritoService: CarritoService) {}
 
-  ngOnInit() {}
-
-  irListaCarrito() {
-
-    this.navCtrl.navigateForward('lista-carrito', { animated: false });
-  
+  ngOnInit() {
+    // Cargar los productos del carrito al inicializar el componente
+    this.cargarCarrito();
   }
 
+  // Método para navegar a la vista de lista de carrito
+  irListaCarrito() {
+    this.navCtrl.navigateForward('lista-carrito', { animated: false });
+  }
+
+  // Método para cargar los productos del carrito y calcular el total
+  cargarCarrito() {
+    this.productosCarrito = this.carritoService.getProductosCarrito();
+    this.calcularTotal();
+  }
+
+  // Método para calcular el total del carrito
+  calcularTotal() {
+    this.totalCarrito = this.carritoService.calcularTotal();
+  }
 }
